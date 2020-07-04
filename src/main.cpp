6fc09e36792f7f2b39807ff5633aa01a22c15285
Vladimir Shishov <wladimir.shishov@gmail.com>
2020-07-04 22:53:27 +0300

    add check port

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
 
+class InvalidInteger : public std::logic_error
+{
+public:
+  InvalidInteger(const std::string& a_message)
+    : logic_error(a_message)
+  { }
+};
+
+long GetIntFromArgv(const char* argv)
+{
+  long lValue{0};
+  try {
+    lValue = std::stoi(argv);
+  }
+  catch (const std::invalid_argument& ex) {
+    throw InvalidInteger("Number is incorrect");
+  }
+  catch (const std::out_of_range& ex) {
+    throw InvalidInteger("Number is out of range");
+  }
+
+  if (lValue == 0) {
+    throw InvalidInteger("Number must be greater then zero");
+  }
+
+  if (lValue < 0) {
+    throw InvalidInteger("Number must not be negative");
+  }
+
+  return lValue;
+}
 
 int main(int argc, char** argv)
 {
   if (argc < 2) {
     std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
     return 1;
+  }
+
+  try {
+    int nPort = GetIntFromArgv(argv[1]);
+    std::cout << nPort << std::endl;
+  }
+  catch(const InvalidInteger& ex) {
+    std::cerr << "Port is invalid" << std::endl;
+    std::cerr << "Error: "<< ex.what() << std::endl;
+  } 
+  catch(const std::exception& ex) {
+    std::cerr << ex.what() << std::endl;
   }
 
   return 0;
